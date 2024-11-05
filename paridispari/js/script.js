8aef0e667613f9a4e1121eb5dba6441268531f57
DeserioArianna <arianna.deserio@gmail.com>
2024-11-05 19:11:05 +0100

    feat:pari dispari finalmente spero funzionante

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -9,27 +9,31 @@ function pariDispari(numeroUtente) {
     // Sommare il numero dell'utente con il numero del computer
     const somma = numeroUtente + numeroComputer;
 
-    // Controlla se il numero dell'utente è pari o dispari
+    // Controlla se il numero della somma è pari o dispari
+    let sommaPari;
     if (somma % 2 === 0) {
         console.log(`${somma} è pari`);
+        sommaPari = true;
     } else {
-        console.log(`${somma} è dispari`)
+        console.log(`${somma} è dispari`);
+        sommaPari = false;
     }
 
     // Restituisce i numeri generati come oggetto
     return {
         numeroComputer: numeroComputer,
-        somma: somma
+        somma: somma,
+        sommaPari: sommaPari
     };
 }
 
 // Esegui la funzione e salva il risultato
-const risultati = pariDispari(numeroUtente);
+const risultati = pariDispari(numeroUtente, userOpinion);
 console.log(`Il numero generato dall'utente: ${numeroUtente}, il numero generato dal computer: ${risultati.numeroComputer}`);
 console.log(`Somma: ${risultati.somma}`);
 
 // Controlla se l'opinione dell'utente corrisponde al risultato
-if (risultati.somma && userOpinion === "pari") {
+if ((risultati.sommaPari && userOpinion === "pari") || (!risultati.sommaPari && userOpinion === "dispari")) {
     console.log("Hai vinto!");
 } else {
     console.log("Hai perso!");
